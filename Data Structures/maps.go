8eaba95a2238c8160a148f75d0d5fde47b5acd7d
rednafi <redowan.nafi@gmail.com>
2020-05-02 02:37:03 +0600

    Added maps

---
+++ Data Structures/maps.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Make a map where the keys are strings & the values are slices of integers
+	m := make(map[string][]int)
+	fmt.Println("empty map:", m)
+
+	// Assign values to the map
+	m["list1"] = []int{0,1,2,3}
+	m["list2"] = []int{4,5,6,7}
+
+	// Print the map after assignment
+	fmt.Println("original map", m)
+
+	// Print length of the map (number of keys)
+	fmt.Println("length of map:", len(m))
+
+
+	// Delete
+	delete(m, "list1")
+	fmt.Println("deleted list1:", m)
+
+	// Check if key list1 exists
+	_, exists := m["list1"]
+	print("list1 exists:", exists)
+}
--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 ## Data Structures
 1. [Go by Examples](https://gobyexample.com/)
 
+## Blogs
 ### Arrays & Slices
 * [Arrays, slices (and strings): The mechanics of 'append'](https://blog.golang.org/slices)
 
 * [Go Slices: Usage & Internals](https://blog.golang.org/slices-intro)
 
-## Idioms
-
-1. [Idiomatic Go](https://about.sourcegraph.com/go/idiomatic-go)
+### Idioms
+* [Idiomatic Go](https://about.sourcegraph.com/go/idiomatic-go)
